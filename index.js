815cd7e44d9d6cc0ae66b47205940cc4e607b52a
Tomasz Wiszowaty <twiszowaty@hotmail.com>
2019-09-29 14:35:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function logShout(string){
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 
 function whisper(string){
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logWhisper(string){
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(String) 
-  if ((string) ==== lowercase){
+  if ((string)====lowercase) {
   return "I can't hear you!"
 }
 
 
-  if ((string)===lowercase) {
-    return "I can't hear you!"
-  }
