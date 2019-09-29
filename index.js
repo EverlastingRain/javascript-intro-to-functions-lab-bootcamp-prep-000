2f58f52b054877d31468ebddf393db4da1ce0c6d
Tomasz Wiszowaty <twiszowaty@hotmail.com>
2019-09-29 14:16:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ function logShout(string) {
 
 function shout(string) {
   return string.toUpperCase()
-};
+}
 
 function whisper(string) {
   return string.toLowerCase()
-};
+}
 function logShout(string) {
   console.log(string.toUpperCase())
-};
+}
 
 function logWhisper(string) {
   console.log(string.toLowerCase())
-};
+}
